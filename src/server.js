8ecfbe667df26789cad2656e5f96383b4e1bc7f3
mdazharuddin1011999 <mdazharuddin1011999@gmail.com>
2021-05-04 10:50:57 +0530

    Removed comments

--- src/server.js
+++ src/server.js
@@ -3,61 +3,6 @@ const app = require("./app");
 
 server = http.createServer(app);
 
-server.listen(3000, ()=>{
-    console.log("Server started at http://localhost:3000");
+server.listen(process.env.PORT, ()=>{
+    console.log(`Server started at http://localhost:${process.env.PORT}`);
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const firebase = require("firebase");
-
-// const port = 3000;
-// var firebaseConfig = {
-//     apiKey: "AIzaSyBxB0-vme5VsZxe0K-VlCOoxtN3hsTpbG0",
-//     authDomain: "test-864ca.firebaseapp.com",
-//     projectId: "test-864ca",
-//     storageBucket: "test-864ca.appspot.com",
-//     messagingSenderId: "637039823120",
-//     appId: "1:637039823120:web:bd54d2faa562727dec7d95",
-//     measurementId: "G-0PK0NQJ2P0"
-// };
-
-// firebase.initializeApp(firebaseConfig);
-// let database = firebase.database();
-// const app = express();
-
-// app.get('/', (req, res)=>{
-//     res.send("Hello World");
-// });
-
-// app.get('/write', (req, res)=>{
-//     database.ref("test").set({"hello": "test"}, err=>{
-//         if(err){
-//             console.error("Error: ", err);
-//         }else{
-//             console.log("Success");
-//         }
-//     });
-//     res.send("writing...")
-// });
-
-// app.listen(port, ()=>{
-//     console.log(`App running at http://localhost:${port}`);
-// });
